fdb83ecc082e799d84c01c639eb408f519a6f42b
Hassaan <hassaan_bk@outlook.com>
2023-03-18 15:18:26 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.send('this is the homepage');
+})
+
+app.get("/healthy", (req, res) => {
+    res.send('healthy');
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server running at ${port}`);
+});
+module.exports = app;
